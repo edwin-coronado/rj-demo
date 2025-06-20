bae7d9a17e7072fcca449569f49cd85aacebc0a1
System Administrator <admin@example.com>
2025-06-20 15:38:22 -0700

    added the keys

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -4,6 +4,24 @@ declare global {
     namespace Now {
         namespace Internal {
             interface Keys extends KeysRegistry {
+                explicit: {
+                        "cs0": {
+                            "table": "sys_script_client",
+                            "id": "a3c651e0fbea4ba09cefddb5e3281d88"
+                        },
+                        "src_server_script_ts": {
+                            "table": "sys_module",
+                            "id": "5a50527cd9194120b45988e5cf5447cb"
+                        },
+                        "br0": {
+                            "table": "sys_script",
+                            "id": "53557ebde47a4d8980b3b157c9d729bc"
+                        },
+                        "package_json": {
+                            "table": "sys_module",
+                            "id": "f65940c2278f4baea08f6f30bd190537"
+                        }
+                    };
             }
         }
     }
