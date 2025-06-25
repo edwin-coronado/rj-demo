4b406ced504cbeef8f8309fd764ec92f2971b490
System Administrator <admin@example.com>
2025-06-25 09:46:53 -0400

    Added "Price" column

--- src/fluent/tables.now.ts
+++ src/fluent/tables.now.ts
@@ -1,9 +1,10 @@
-import { StringColumn, Table } from '@servicenow/sdk/core'
+import { StringColumn, Table, IntegerColumn } from '@servicenow/sdk/core'
 
 export const x_snc_rjapp_restaurant = Table({
     name: "x_snc_rjapp_restaurant",
     display: 'name',
     schema: {
-        name: StringColumn({ label: 'Restaurant', mandatory: true })
+        name: StringColumn({ label: 'Restaurant', mandatory: true }),
+        price: IntegerColumn({ label: 'Price'})
     }
 })
